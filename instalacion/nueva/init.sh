597276b6ea914d5ce73ac61f3f1c06aeca6e426e
Ubuntu <ubuntu@ip-172-31-11-249.eu-west-1.compute.internal>
2021-04-29 11:19:53 +0000

    seguridad metricbeat setup

--- instalacion/nueva/init.sh
+++ instalacion/nueva/init.sh
@@ -1,9 +1,9 @@
-sed -e "s/\${IP}/172.31.11.249/" apache/heartbeat/heartbeat.template.yml > apache/heartbeat/heartbeat.yml
-sed -e "s/\${IP}/172.31.11.249/" apache/heartbeat/heartbeat.template.yml > apache/heartbeat/heartbeat.yml
-sed -e "s/\${IP}/172.31.11.249/" apache/docker-compose.template.yaml > apache/docker-compose.yml
-sed -e "s/\${IP}/172.31.11.249/" apache/docker-compose.template.yaml > apache/docker-compose.yml
-sed -e "s/\${IP}/172.31.11.249/" elastic-kibana/instances.template.yml > elastic-kibana/instances.yml
-sed -e "s/\${IP}/172.31.11.249/" logstash/pipelines/pipeline_heartbeat.template.conf > logstash/pipelines/pipeline_heartbeat.conf
+export IP="172.31.11.249"
+
+sed -e "s/\${IP}/$IP/g" apache/heartbeat/heartbeat.template.yml > apache/heartbeat/heartbeat.yml
+sed -e "s/\${IP}/$IP/g" apache/docker-compose.template.yaml > apache/docker-compose.yml
+sed -e "s/\${IP}/$IP/g" elastic-kibana/instances.template.yml > elastic-kibana/instances.yml
+sed -e "s/\${IP}/$IP/g" logstash/pipelines/pipeline_heartbeat.template.conf > logstash/pipelines/pipeline_heartbeat.conf
 
 
 # Arrancamos el cluster de Elastic y Kibana
